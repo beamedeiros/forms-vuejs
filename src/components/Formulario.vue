d0b478e441e4411e463724aab9aeb86be10b7e1d
Beatriz Santos <beatriz.santos@prima.com.br>
2023-03-06 14:57:55 -0300

    biblioteca moment e formatação de data/ hora

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -151,31 +151,31 @@
                     <div class="mb-3 row">
                         <label class="col-3 col-form-label">Data:</label>
                         <div class="col">
-                            <input type="date" class="form-control">
+                            <input type="date" class="form-control" v-model="form.data">
                         </div>
                     </div>
                     <div class="mb-3 row">
                         <label class="col-3 col-form-label">Data/hora local:</label>
                         <div class="col">
-                            <input type="datetime-local" class="form-control">
+                            <input type="datetime-local" class="form-control" v-model="form.dataHoraLocal">
                         </div>
                     </div>
                     <div class="mb-3 row">
                         <label class="col-3 col-form-label">Mês:</label>
                         <div class="col">
-                            <input type="month" class="form-control">
+                            <input type="month" class="form-control" v-model="form.mes">
                         </div>
                     </div>
                     <div class="mb-3 row">
                         <label class="col-3 col-form-label">Semana:</label>
                         <div class="col">
-                            <input type="week" class="form-control">
+                            <input type="week" class="form-control" v-model="form.semana">
                         </div>
                     </div>
                     <div class="mb-3 row">
                         <label class="col-3 col-form-label">Hora:</label>
                         <div class="col">
-                            <input type="time" class="form-control">
+                            <input type="time" class="form-control" v-model="form.hora">
                         </div>
                     </div>
                     <div class="mb-3 row">
@@ -275,19 +275,45 @@
                     <span>RG: {{ form.rg }}</span>
                 </div>
                 <div class="mb-3 row">
-                    <span>Data:</span>
+                    <span>Data: {{ form.data }} | {{ $moment(form.data).format('DD/MM/YYYY') }}</span>
                 </div>
                 <div class="mb-3 row">
-                    <span>Data/hora local:</span>
+                    <span>Data/hora local: {{ form.dataHoraLocal }}</span>
+                    <ul>
+                        <li>
+                            {{ $moment(form.dataHoraLocal).format('dddd') }}
+                        </li>
+                        <li>
+                            {{ $moment(form.dataHoraLocal).add(10, 'days').format('LL') }}
+                        </li>
+                        <li>
+                            {{ $moment(form.dataHoraLocal).add(3, 'months').format('LL') }}
+                        </li>
+                        <li>
+                            {{ $moment(form.dataHoraLocal).add(2, 'years').format('LL') }}
+                        </li>
+                        <li>
+                            {{ $moment(form.dataHoraLocal).subtract(10, 'days').format('LL') }}
+                        </li>
+                        <li>
+                            {{ $moment(form.dataHoraLocal).subtract(3, 'months').format('LL') }}
+                        </li>
+                        <li>
+                            {{ $moment(form.dataHoraLocal).subtract(2, 'years').format('LL') }}
+                        </li>
+                        <li>
+                            {{ $moment(form.dataHoraLocal).add(2, 'days').format('LLLL') }}
+                        </li>
+                    </ul>
                 </div>
                 <div class="mb-3 row">
-                    <span>Mês:</span>
+                    <span>Mês: {{ form.mes }}</span>
                 </div>
                 <div class="mb-3 row">
-                    <span>Semana:</span>
+                    <span>Semana: {{ form.semana }}</span>
                 </div>
                 <div class="mb-3 row">
-                    <span>Hora:</span>
+                    <span>Hora: {{ form.hora }}</span>
                 </div>
                 <div class="mb-3 row">
                     <span>Cor:</span>
@@ -308,9 +334,12 @@
 </template>
 
 <script>
+import  moment from 'moment'
+
 export default {
     name: 'FormPage',
     data: () => ({
+        moment: {},
         form: {
             nome: '',
             email: '',
@@ -327,7 +356,15 @@ export default {
             placaVeiculo: '',
             placaVeiculoMercosul: '',
             rg: '',
+            data: '',
+            dataHoraLocal: '',
+            mes: '',
+            semana: '',
+            hora: ''
         },
     }),
+    created() {
+        this.moment = moment
+    }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import Maska from 'maska'
+import moment from 'moment'
 
 
 //createApp(App).mount('#app')
 const app = createApp(App)
-app.use(Maska)
+
+app.use(Maska) //plugin
+app.config.globalProperties.$moment = moment //propriedade global
+app.config.globalProperties.$moment.locale('pt-br') 
+
 app.mount('#app')
